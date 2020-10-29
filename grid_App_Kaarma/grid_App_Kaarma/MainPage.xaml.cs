35e7b14416536de220ad5b65165e078bef74d274
Mark Kaarma <mark.kaarma@gmail.com>
2020-10-29 19:57:04 +0200

    add new button Update

--- grid_App_Kaarma/grid_App_Kaarma/MainPage.xaml.cs
+++ grid_App_Kaarma/grid_App_Kaarma/MainPage.xaml.cs
@@ -2,56 +2,88 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 
-namespace grid_App_Kaarma
+namespace Grid_App
 {
     public partial class MainPage : ContentPage
     {
-        BoxView box;
-        Button new_game, random_player;
+        //BoxView box;
+        BoxView[,] boxs = new BoxView[5, 5];
+        int i, j;
+        Image img;
         public MainPage()
         {
-            New_Game_Clicked();
-        }
-        void New_Game_Clicked()
-        {
-            Grid grid = new Grid(); // Цикл что бы не расписывать, как в xaml 
-            for (int i = 0; i < 4; i++)
+            /* Grid grid = new Grid
+            {
+                RowDefinitions =
+                {
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)}
+                },
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                }
+            }; */
+            Grid grid = new Grid();
+            for (int i = 0; i < 5; i++)
             {
                 grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
                 grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
-            for (int i = 0; i < 3; i++)
+            for (i = 0; i < 5; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (j = 0; j < 5; j++)
                 {
-                    box = new BoxView { Color = Color.FromRgb(355, 0, 124) };
-                    grid.Children.Add(box, i, j);
+                    //boxs[i,j] = new BoxView { Color = Color.FromRgb(200, 100, 50) };//box->array
+                    img = new Image { Source=ImageSource.FromFile("nolik.png")};
+
+                    //grid.Children.Add(boxs[i,j], i, j
+                    grid.Children.Add(img, i, j);
                     var tap = new TapGestureRecognizer();
-                    box.GestureRecognizers.Add(tap);
-                    tap.Tapped += Tap_Tapped;
+                    //tap.Tapped += Tap_Tapped;
+                    //boxs[i,j].GestureRecognizers.Add(tap);
+                    img.GestureRecognizers.Add(tap);
+                    tap.Tapped += async (object sender, EventArgs e) =>
+                     {
+                    //BoxView box = sender as BoxView;
+                    //if (box.Color==new Color(0,0,0))
+                    //{
+                    //    box.Color = Color.FromRgb(200, 100, 50);
+                    //}
+                    //else
+                    //{
+                    //    box.Color = new Color(0, 0, 0);
+                    //}
+                    Image img = sender as Image;
+                    if (img.Source==ImageSource.FromFile("nolik.png"))
+                    {
+                        img.Source = ImageSource.FromFile("krestik.png");
+                    }
+                    else
+                    {
+                        img.Source = ImageSource.FromFile("nolik.png");
+                    }
+                    };
                 }
             }
-            new_game = new Button { Text = "Обнулить" };
-            grid.Children.Add(new_game, 0, 3);
-            Grid.SetColumnSpan(new_game, 2);
-            random_player = new Button { Text = "Крестик или нолик?" };
-            grid.Children.Add(random_player, 2, 3);
-            Grid.SetColumnSpan(random_player, 2);
             Content = grid;
         }
-
-
-        private void Tap_Tapped(object sender, EventArgs e)
-        {
-            BoxView box = sender as BoxView;
-            box.Color = Color.Black;
-        }
+        //private void Tap_Tapped(object sender, EventArgs e)
+        //{
+        //    BoxView box = sender as BoxView;
+        //    box.Color = Color.FromRgb(0, 0, 0);
+        //}
     }
-
 }
-
